cb9ce7ad07cae623d8e976fc6e4ab4ef29cb7cf9
Marvin Heilemann <marvin.heilemann@googlemail.com>
2020-02-05 19:00:48 +0100

    fix: pnpm plugin now resolve all node modules
    closes #40

--- gatsby/node/onCreateWebpackConfig.js
+++ gatsby/node/onCreateWebpackConfig.js
@@ -1,20 +1,11 @@
-const path = require('path')
 const { getVersion } = require('../../utils/version')
 const { isProd } = require('../../utils/environment')
-
-// BUG: https://github.com/webpack/webpack/issues/5087
-// BUG: https://github.com/Js-Brecht/gatsby-plugin-pnpm/issues/1
-const nodePath = (process.env.NODE_PATH &&
-  process.env.NODE_PATH.split(path.delimiter)) || [path.join(__dirname, 'node_modules')]
 
 module.exports = async ({ actions, plugins }) => {
   const { setWebpackConfig } = actions
 
   await setWebpackConfig({
     devtool: isProd ? false : 'cheap-module-source-map',
-    resolveLoader: {
-      modules: nodePath,
-    },
     plugins: [
       plugins.define({
         'process.env': {
