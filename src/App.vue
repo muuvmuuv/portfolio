9e260d2f444c5f5599ef1f8bdebd2ad40f6bf355
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2024-12-15 17:44:19 +0100

    docs: update readme otf

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { Analytics } from '@vercel/analytics/vue';
-import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { Analytics } from "@vercel/analytics/vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
