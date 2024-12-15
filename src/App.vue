348827f15a0440eb153ed3c496f651c989ac1c06
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2024-12-15 17:35:26 +0100

    feat: add vercel speed and analytics

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from "@vercel/speed-insights/vue"
+
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
 </script>
@@ -11,4 +14,7 @@ import Header from "./components/Header.vue";
 	</main>
 
 	<Footer />
+
+	<SpeedInsights />
+	<Analytics />
 </template>
